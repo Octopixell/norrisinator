a9ee80d59c37f4faa3a1703a34c8fdb436debf4a
Octopixell <mr.strijker@gmail.com>
2020-02-09 13:01:12 +0100

    Whitespace be gone!

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { JokesComponent } from './jokes/jokes.component';
 
-
 const routes: Routes = [
   { path: '', component: JokesComponent },
   { path: '**', component: JokesComponent }
