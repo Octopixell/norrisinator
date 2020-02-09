cc58defeb0fd1ec6a3fc60d761d29884bf007402
Octopixell <mr.strijker@gmail.com>
2020-02-09 12:46:47 +0100

    Use the JokeService to retrieve jokes and store them in our app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { JokeService } from './shared/joke.service';
+import { Joke } from './interfaces/joke';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [JokeService]
 })
-export class AppComponent {
-  title = 'norrisinator';
+export class AppComponent implements OnInit, OnDestroy {
+  jokes: Joke[] = [];
+  jokeSub: Subscription;
+
+  constructor(private jokeService: JokeService) { }
+
+  ngOnInit(): void {
+    this.jokeSub = this.jokeService.getRandomJokes().subscribe((response) => {
+      if (response.type === 'success') {
+        this.jokes = response.value;
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.jokeSub) { this.jokeSub.unsubscribe(); }
+  }
 }
