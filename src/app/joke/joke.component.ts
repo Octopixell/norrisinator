2b55f96f61c12c426748feaa5afcd41426f4a52b
Octopixell <mr.strijker@gmail.com>
2020-02-09 23:38:30 +0100

    Add basic functionality to add a joke to a list of favorites which is held in the JokeService to keep the list central

--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Joke } from '../interfaces/joke';
+import { JokeService } from '../shared/joke.service';
 
 @Component({
   selector: 'app-joke',
@@ -9,8 +10,12 @@ import { Joke } from '../interfaces/joke';
 export class JokeComponent implements OnInit {
   @Input() joke: Joke;
 
-  constructor() { }
+  constructor(private jokeService: JokeService) { }
 
   ngOnInit() {
+  }
+
+  markAsFavorite(): void {
+    this.jokeService.markJokeAsFavorite(this.joke);
   }
 }
--- src/app/shared/joke.service.ts
+++ src/app/shared/joke.service.ts
@@ -1,16 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { JokeApiResponse } from '../interfaces/joke-api-response';
+import { Joke } from '../interfaces/joke';
 
 @Injectable({
   providedIn: 'root'
 })
 export class JokeService {
+  private readonly xFavorites = new BehaviorSubject<Joke[]>([]);
+  readonly $favorites = this.xFavorites.asObservable();
+
   constructor(private http: HttpClient) { }
 
   getRandomJokes(amount: number = 1): Observable<JokeApiResponse> {
     return this.http.get<JokeApiResponse>(`${environment.jokeApiUrl}/random/${amount}`);
+  }
+
+  get favorites(): Joke[] {
+    return this.xFavorites.getValue();
+  }
+
+  set favorites(value: Joke[]) {
+    this.xFavorites.next(value);
+  }
+
+  markJokeAsFavorite(joke: Joke): void {
+    this.favorites = [...this.favorites].concat([joke]);
   }
 }
