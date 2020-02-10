a97cc51aa8ecdbc68cf4ccede6165f40177305eb
Octopixell <mr.strijker@gmail.com>
2020-02-10 20:52:18 +0100

    Wrap the joke text in a P element cause unwrapped text is just bad practice and add a heart icon to the favorite button

--- src/app/joke/joke.component.ts
+++ src/app/joke/joke.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Joke } from '../interfaces/joke';
 import { JokeService } from '../shared/joke.service';
+import { faTrashAlt, faHeart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-joke',
@@ -9,6 +10,7 @@ import { JokeService } from '../shared/joke.service';
 })
 export class JokeComponent implements OnInit {
   @Input() joke: Joke;
+  heartIcon = faHeart;
 
   constructor(private jokeService: JokeService) { }
 
